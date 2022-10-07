e8845f568178081b019d8a6affb701620c2e138f
Gabriel Elias <gabriel_elias9@hotmail.com>
2022-10-07 09:31:46 -0300

    Ajustando protocolo do baseURL da api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://pocs.digitalpages.com.br/mock/api/fruits-api/fruits.json'
+    baseURL: 'https://pocs.digitalpages.com.br/mock/api/fruits-api/fruits.json'
 })
 
 export const useApi = () => ({
